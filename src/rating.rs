8cf4ad3f9afe700b8322c7f17a21abde8103e577
Niklas Fiekas <niklas.fiekas@sartorius.com>
2024-10-30 23:23:31 +0100

    add more types

---
+++ src/rating.rs
@@ -0,0 +1,35 @@
+use std::ops;
+
+use crate::Instant;
+
+/// Number representing playing strength, such that the difference between two
+/// ratings can be used to predict an expected score. Higher is better.
+#[derive(Debug, Copy, Clone, PartialEq, PartialOrd)]
+pub struct RatingScalar(pub f64);
+
+impl From<RatingScalar> for f64 {
+    fn from(RatingScalar(rating): RatingScalar) -> f64 {
+        rating
+    }
+}
+
+/// A difference between two ratings.
+#[derive(Debug, Copy, Clone, PartialEq, PartialOrd, Default)]
+pub struct RatingDifference(pub f64);
+
+impl From<RatingDifference> for f64 {
+    fn from(RatingDifference(difference): RatingDifference) -> f64 {
+        difference
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, PartialOrd)]
+pub struct Volatility(pub f64);
+
+#[derive(Debug, Clone)]
+pub struct Rating {
+    pub rating: RatingScalar,
+    pub deviation: RatingDifference,
+    pub volatility: Volatility,
+    pub at: Instant,
+}
