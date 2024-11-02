8255572e8bf8d05a5aa834eea1e9c9c2187df7b9
Niklas Fiekas <niklas.fiekas@sartorius.com>
2024-11-02 15:23:57 +0100

    produce combined csv

--- research/combine-reports.py
+++ research/combine-reports.py
@@ -5,13 +5,13 @@ import dataclasses
 
 @dataclasses.dataclass
 class Experiment:
-    avg_deviance: float
     min_deviation: float
     max_deviation: float
     default_volatility: float
     tau: float
     first_advantage: float
     rating_periods_per_day: float
+    avg_deviance: float
 
 experiments = []
 
@@ -23,15 +23,16 @@ for line in sys.stdin:
     min_deviation, max_deviation, default_volatility, tau, first_advantage, rating_periods_per_day, avg_deviance = line.split(",")
     experiments.append(
         Experiment(
-            avg_deviance=float(avg_deviance),
             min_deviation=float(min_deviation),
             max_deviation=float(max_deviation),
             default_volatility=float(default_volatility),
             tau=float(tau),
             first_advantage=float(first_advantage),
-            rating_periods_per_day=float(rating_periods_per_day)))
+            rating_periods_per_day=float(rating_periods_per_day),
+            avg_deviance=float(avg_deviance)))
 
 experiments.sort(key=lambda e: e.avg_deviance, reverse=True)
 
-for experiment in experiments:
-    print(experiment)
+print("min_deviation,max_deviation,default_volatility,tau,first_advantage,rating_periods_per_day,avg_deviance")
+for ex in experiments:
+    print(f"{ex.min_deviation},{ex.max_deviation},{ex.default_volatility},{ex.tau},{ex.first_advantage},{ex.rating_periods_per_day},{ex.avg_deviance}")
