cc09b6a4dd81eeaa6f24961ecab9c6ee1cbfc798
Niklas Fiekas <niklas.fiekas@sartorius.com>
2024-11-02 15:20:40 +0100

    add script to combine reports

---
+++ research/combine-reports.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import sys
+import dataclasses
+
+@dataclasses.dataclass
+class Experiment:
+    avg_deviance: float
+    min_deviation: float
+    max_deviation: float
+    default_volatility: float
+    tau: float
+    first_advantage: float
+    rating_periods_per_day: float
+
+experiments = []
+
+for line in sys.stdin:
+    line = line.strip()
+    if not line or line.startswith("#") or line.endswith("avg_deviance"):
+        continue
+
+    min_deviation, max_deviation, default_volatility, tau, first_advantage, rating_periods_per_day, avg_deviance = line.split(",")
+    experiments.append(
+        Experiment(
+            avg_deviance=float(avg_deviance),
+            min_deviation=float(min_deviation),
+            max_deviation=float(max_deviation),
+            default_volatility=float(default_volatility),
+            tau=float(tau),
+            first_advantage=float(first_advantage),
+            rating_periods_per_day=float(rating_periods_per_day)))
+
+experiments.sort(key=lambda e: e.avg_deviance, reverse=True)
+
+for experiment in experiments:
+    print(experiment)
